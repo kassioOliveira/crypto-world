089a77944782062a941f641f16f7ed86bd41ac0a
kassio oliveira <kassiooliveira875@gmail.com>
2023-03-31 11:42:26 -0300

    Iniciando criação do painel

---
+++ src/shared/components/Painel/PainelStyle.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const PainelContainer = styled.div `
+    width: 100%;
+    height: 100%;
+`
+
+export const Box = styled.div`
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+    
+`
