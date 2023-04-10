e4b3cc27b8f4169f877e7ee6a3a73f77d61a11ff
kassio oliveira <kassiooliveira875@gmail.com>
2023-04-10 17:53:17 -0300

    Criação da função do filtro de pesquisa

--- src/shared/components/Search/Search.jsx
+++ src/shared/components/Search/Search.jsx
@@ -1,12 +1,192 @@
-import React from "react"
-import { IconLoader, IconSearch, SearchComponent, SearchInput } from "./SearchStyle";
+import React, { useCallback, useEffect, useMemo, useRef, useState } from "react"
+import { IconLoader, IconSearch, Line, ListSearchItem, ListSearchResults, SearchComponent, SearchInput, SearchItemLink, SearchResultTitle, SearchResultsContainer, SearchResultsSubContainer } from "./SearchStyle";
+import { AssetsService } from "../../services/api/assets/AssetsService";
+import { ErrorBoundary, useErrorBoundary } from "react-error-boundary";
+import { ExchangesService } from "../../services/api/exchanges/ExchangesService";
+import { useDebounce } from "../../hooks/useDebounce";
+import { ErrorPage } from "../Fallback/ErrorPage";
 
 export const Search = () => {
+
+  const { showBoundary } = useErrorBoundary();
+  const { debounce } = useDebounce();
+
+  const inputRef = useRef(null);
+  const resultsRef = useRef(null);
+
+  const [assets, setAssets] = useState([]);
+  const [exchanges, setExchanges] = useState([]);
+  const [searchValues, setSearchValues] = useState({});
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [showResults, setShowResults] = useState(false)
+
+  useEffect(() => {
+
+    AssetsService.getAll().then((response) => {
+      setAssets(response?.data);
+    }).catch((error) => {
+      showBoundary(error);
+    });
+
+    ExchangesService.getAll().then((response) => {
+      setExchanges(response?.data);
+    }).catch((error) => {
+      showBoundary(error);
+    });
+
+  }, [showBoundary]);
+
+
+  const handleShowResults = useCallback((result = true) => {
+    if (result) {
+      setShowResults(value => !value)
+      return;
+    }
+
+    setShowResults(false);
+  }, []);
+
+  useEffect(() => {
+
+    const close = (e) => {
+
+      if (resultsRef.current && !resultsRef.current.contains(e.target)) {
+        if (showResults) {
+          setTimeout(() => {
+            handleShowResults(false);
+          }, [200])
+
+          return;
+        }
+      }
+    }
+
+
+    document.addEventListener('click', close, true);
+    document.addEventListener("mousedown", close, true);
+
+    return () => {
+      document.removeEventListener('click', close, true);
+      document.removeEventListener("mousedown", close, true);
+    }
+
+  }, [resultsRef, handleShowResults,showResults]);
+
+
+
+  const getSearch = useCallback(() => {
+
+    if (!isLoading) {
+      setIsLoading(true);
+    }
+    handleShowResults(false)
+
+    debounce(() => {
+
+      const query = inputRef.current.value;
+      const assetsResults = assets.filter((item) => {
+        return String(item.id).toLocaleLowerCase().indexOf(query.toLocaleLowerCase()) !== -1;
+      });
+
+      const exChangesResults = exchanges.filter((item) => {
+        return String(item.exchangeId).toLocaleLowerCase().indexOf(query.toLocaleLowerCase()) !== -1;
+      })
+
+      if ((assetsResults.length > 0 || exChangesResults.length > 0) && query.length > 0) {
+        handleShowResults();
+      }
+
+
+      const value = {
+        assetsResult: assetsResults.length > 5 ? [...assetsResults.slice(0, 5)] : [...assetsResults],
+        exchangesResult: exChangesResults.length > 5 ? [...exChangesResults.slice(0, 5)] : [...exChangesResults]
+      }
+
+      setSearchValues(value);
+      setIsLoading(false)
+
+
+
+
+    });
+
+
+
+
+
+
+
+
+  }, [assets, exchanges,isLoading,debounce,handleShowResults]);
+
+
   return (
-    <SearchComponent>
-        <SearchInput type="text"/>
-        <IconSearch />
-        {/* <IconLoader/> */}
+    <ErrorBoundary fallback={<ErrorPage/>}>
+      <SearchComponent>
+      <SearchInput ref={inputRef} onChange={getSearch} type="text" />
+      {isLoading ? (<IconLoader />) : (<IconSearch />)}
+          <SearchResultsContainer d={{showResults}} ref={resultsRef}>
+
+            {
+             searchValues && searchValues.assetsResult?.length > 0 && (
+                <SearchResultsSubContainer>
+                <SearchResultTitle>
+                    Cryptos
+                  </SearchResultTitle>
+                  <ListSearchResults>
+
+                    {
+                      (searchValues.assetsResult?.map((item) => {
+                        return (
+                         <SearchItemLink key={item.rank} to={`/cryptos/${item.id}`}>
+                           < ListSearchItem >
+                            {item.id}
+                          </ListSearchItem>
+                         </SearchItemLink>
+                        )
+                      }))
+                    }
+
+                  </ListSearchResults>
+
+                </SearchResultsSubContainer>
+              )
+            }
+
+
+
+            {searchValues.exchangesResult?.length > 0 && searchValues.assetsResult?.length > 0 && (
+                <Line />
+              )}
+
+            {
+              searchValues.exchangesResult?.length > 0 && (
+                <SearchResultsSubContainer>
+                  <SearchResultTitle>
+                    Corretoras
+                  </SearchResultTitle>
+                  <ListSearchResults>
+                    {(searchValues.exchangesResult?.map((item) => {
+                      return (
+                        <SearchItemLink key={item.rank} to={`/corretoras/${item.exchangeId}`} >
+                          <ListSearchItem >
+                          {item.exchangeId}
+                        </ListSearchItem>
+                        </SearchItemLink>
+                      )
+                    }))}
+
+                  </ListSearchResults>
+
+                </SearchResultsSubContainer>
+              )
+            }
+
+
+          </SearchResultsContainer>
     </SearchComponent>
+    </ErrorBoundary>
   )
 }
--- src/shared/components/Search/SearchStyle.jsx
+++ src/shared/components/Search/SearchStyle.jsx
@@ -2,6 +2,7 @@ import styled, {keyframes} from "styled-components";
 import {FaSearch} from "react-icons/fa";
 
 import {BiLoaderAlt} from "react-icons/bi";
+import { Link } from "react-router-dom";
 
 
 
@@ -9,6 +10,7 @@ export const SearchComponent = styled.form`
  
     height: 30px;
     width: 200px;
+    position: relative;
     border-radius:5px;
     display: flex;
     align-items: center;
@@ -72,4 +74,68 @@ font-size: 15px;
 } 
 
 `;
+
+
+export const SearchResultsContainer = styled.div`
+  width: 100%;
+  height: fit-content;
+  padding: 10px;
+  position: absolute;
+  top: 105%;
+  z-index: 100;
+  display: ${({d})=> d && !d.showResults?"none":"flex"};
+  flex-direction: column;
+  align-items: center;
+  background-color: #ffff;
+justify-content: space-around;
+gap: 10px;
+box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+`
+
+export const SearchResultTitle = styled.span`
+  font-weight: bold;
+  font-size: 1.3rem;
+`
+
+export const SearchResultsSubContainer = styled.div`
+  width: 90%;
+  height: 150px;
+  display: flex;
+flex-direction:column;
+justify-content: space-around;
+align-items: center;
+ 
+`
+
+export const ListSearchResults = styled.ul`
+width: 100%;
+height: 80%;
+display: flex;
+flex-direction:column;
+gap: 3px;
+  
+`
+export const ListSearchItem = styled.li`
+  width: 100%;
+  height: 100%;
+  list-style: none;
+  text-decoration:none;
+`
+export const SearchItemLink = styled(Link)`
+  width: 100%;
+  height: 20px;
+color: gray;
+font-weight:bold;
+  list-style: none;
+  text-decoration:none;
+  text-align: left;
+`
+
+export const Line = styled.hr`
+  width: 90%;
+  color: black;
+  height: 2px;
+  font-size:4px;
+  display: block;
+`
 
